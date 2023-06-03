93a34e619b05f01816aeb8ece4b99c8cc6be987a
CvilleTerp <andrewfthornton@gmail.com>
2023-06-03 13:59:07 -0400

    sankey diagram push

--- j_notebooks/app.py
+++ j_notebooks/app.py
@@ -3,26 +3,47 @@ import plotly.graph_objects as go
 
 import pandas as pd
 
-app = Dash(__name__)
 
 df = (pd.read_csv("https://raw.githubusercontent.com/andrewt-cville/fb_recruiting/master/j_notebooks/sankey_output.csv")).set_index('college')
 
-fig = go.Figure(data=[go.Sankey(
-    node = dict(
-      pad = 15,
-      thickness = 20,
-      line = dict(color = "black", width = 0.5),
-      label = ["Five Stars", "Four Stars", "Three Stars", "Two Stars", "Unranked", "Drafted", "Undrafted", "NFL Game Participation"],
-      color = "blue"
-    ),
-    link = dict(
-      source = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 5, 6], # indices correspond to labels, eg A1, A2, A1, B1, ...
-      target = [5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7],
-      value = (df.loc['maryland']).values.tolist()
-  ))])
+app = Dash(__name__)
 
-fig.update_layout(title_text="Maryland Recruits 2002-2017", font_size=24)
-fig.show()
+app.layout = html.Div([
+    html.H1('College Recruiting Results: 2002 - 2017'),
+
+    dcc.Dropdown(
+        df.index.unique(),
+        'maryland',
+        id='college'),
+
+    dcc.Graph(id='sankey')
+])
+
+@app.callback(
+    Output('sankey', 'figure'),
+    Input('college', 'value')
+)
+
+def update_sankey(college):
+
+    fig = go.Figure(data=[go.Sankey(
+        node = dict(
+        pad = 15,
+        thickness = 20,
+        line = dict(color = "black", width = 0.5),
+        label = ["Five Stars", "Four Stars", "Three Stars", "Two Stars", "Unranked", "Drafted", "Undrafted"],
+        color = "blue"
+        ),
+        link = dict(
+        source = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4], # indices correspond to labels, eg A1, A2, A1, B1, ...
+        target = [5, 5, 5, 5, 5, 6, 6, 6, 6, 6],
+        value = (df.loc[college]).values.tolist()
+    ))])
+
+    fig.update_layout(font_size=24)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
